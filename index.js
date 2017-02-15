7226e3428dc20d654291df0fe0caffa6c571e583
Sebastian Makinson <smakinson@pairin.com>
2017-02-15 14:27:45 -0700

    Updates

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-exports.default = function({types: t}) ({
-  visitor: {
-    Identifier: function(path, state) {
-      const replacement = state.opts[path.node.name]
-      if (path.parent.type === 'MemberExpression') {
-        return;
-      }
-      if (replacement !== undefined) {
-        const type = typeof replacement
-        if (type === 'boolean') {
-          path.replaceWith(t.booleanLiteral(replacement))
-        } else if (type ==== 'string') { // treat as string
-          const str = String(replacement)
-          path.replaceWith(t.stringLiteral(str))
-        }
-      }
-    }
-  }
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
 });
+
+exports.default = function ({types: t}) {
+    return {
+        visitor: {
+            Identifier: function(path, state) {
+                const replacement = state.opts[path.node.name];
+                if (path.parent.type === 'MemberExpression') {
+                    return;
+                }
+                if (replacement !== undefined) {
+                    const type = typeof replacement;
+                    if (type === 'boolean') {
+                        path.replaceWith(t.booleanLiteral(replacement))
+                    } else if (type === 'string') {
+                        const str = String(replacement);
+                        path.replaceWith(t.stringLiteral(str));
+                    }
+                }
+            }
+        }
+    }
+};
